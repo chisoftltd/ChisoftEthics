3d8e3b10932efe7811f08702057bff54b5277ec2
chisoftltd <chisoftltd@gmail.com>
2017-04-21 21:22:42 +0100

    Further improvement

--- student.php
+++ student.php
@@ -105,7 +105,7 @@ VALUES('" . $name . "', '" . $supervisor . "', '" . $department . "','" . $proje
                 <?php if (isset($_SESSION['usr_id'])) { ?>
                     <li class="active"><a href="signinindex.php">Home</a></li>
                     <li><a href="student.php">Student</a></li>
-                    <li><a href="EAO.php">Experiment Approval Officers (EAO)</a></li>
+                    <li><a href="eao.php">Experiment Approval Officers (EAO)</a></li>
                     <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
                     <li><a href="logout.php">Log Out</a></li>
                 <?php } else { ?>
