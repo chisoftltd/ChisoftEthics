b49eb7742c2e827c1d5e79d41801ff49a782e2e1
chisoftltd <chisoftltd@gmail.com>
2017-04-23 23:26:22 +0100

    further improvment

--- researcher.php
+++ researcher.php
@@ -130,7 +130,7 @@ VALUES('" . $name . "', '" . $supervisor . "', '" . $department . "','" . $proje
 <div class="container">
 
 
-    <h2>Modal Login Form</h2>
+    <h2>Research Ethics Management System | Create | Update | Delete</h2>
 
     <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Create</button>
 
