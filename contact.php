2328718bee74ae40862c00b0f7ec18e6dc6968f5
chisoftltd <chisoftltd@gmail.com>
2017-04-23 14:47:09 +0100

    Further improvement

--- contact.php
+++ contact.php
@@ -119,33 +119,6 @@ if (isset($_POST["submit"])) {
                     <div id="map-container" class="col-md-8"></div>
                 </div><!-- /map-outer -->
             </div> <!-- /row -->
-
-            <div class="row">
-                <form class="form-horizontal" name="commentform">
-                    <div class="form-group">
-                        <div class="col-md-4">
-                            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name"/>
-                        </div>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name"/>
-                        </div>
-                        <div class="col-md-4 input-group">
-                            <span class="input-group-addon">@</span>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email Address"/>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <textarea rows="6" class="form-control" id="comments" name="comments" placeholder="Your question or comment here"></textarea>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-6">
-                            <button type="submit" value="Submit" class="btn btn-warning pull-right">Send</button>
-                        </div>
-                    </div>
-                </form>
-            </div><!-- /row -->
         </form>
     </div><!-- /container -->
     <form>
