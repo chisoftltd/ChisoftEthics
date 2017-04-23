5a4a78b90c5931451e9c0be956dad3ac25706d62
chisoftltd <chisoftltd@gmail.com>
2017-04-23 14:52:51 +0100

    Further improvement

--- contact.php
+++ contact.php
@@ -55,6 +55,11 @@ if (isset($_POST["submit"])) {
         <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
         <script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
         <![endif]-->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
     </head>
     <body>
     <nav class="navbar navbar-default" role="navigation">
@@ -117,6 +122,31 @@ if (isset($_POST["submit"])) {
                         </address>
                     </div>
                     <div id="map-container" class="col-md-8"></div>
+                    <script>
+
+                        function init_map() {
+                            var var_location = new google.maps.LatLng(45.430817,12.331516);
+
+                            var var_mapoptions = {
+                                center: var_location,
+                                zoom: 14
+                            };
+
+                            var var_marker = new google.maps.Marker({
+                                position: var_location,
+                                map: var_map,
+                                title:"Venice"});
+
+                            var var_map = new google.maps.Map(document.getElementById("map-container"),
+                                var_mapoptions);
+
+                            var_marker.setMap(var_map);
+
+                        }
+
+                        google.maps.event.addDomListener(window, 'load', init_map);
+
+                    </script>
                 </div><!-- /map-outer -->
             </div> <!-- /row -->
         </form>
@@ -172,10 +202,6 @@ if (isset($_POST["submit"])) {
     <footer>
         <?php include 'include/footer.php'; ?>
     </footer>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <!-- Latest compiled JavaScript -->
