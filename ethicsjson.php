b36aac3cf7c7bde743fbbaf09a34b039ff8d82a4
chisoftltd <chisoftltd@gmail.com>
2017-05-02 23:03:30 +0100

    further improvement

--- ethicsjson.php
+++ ethicsjson.php
@@ -73,7 +73,7 @@
     $rows = array();
     if (mysqli_num_rows($result2)){
         while ($row=mysqli_fetch_array($query)){
-            $rows[] = $row. " ";
+            $rows[] = $row;
         }
         echo json_encode($rows);
     }
