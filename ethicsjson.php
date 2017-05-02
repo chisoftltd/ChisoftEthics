de091a39cd090efe354a329681b9f9cc53a837bb
chisoftltd <chisoftltd@gmail.com>
2017-05-02 22:55:35 +0100

    further improvement

--- ethicsjson.php
+++ ethicsjson.php
@@ -1,31 +1,9 @@
-<?php
 /**
- * Created by PhpStorm.
- * User: Chisoft
- * Date: 2017-05-02
- * Time: 15:02
- */
-// Start a session
-ob_start();
-session_start();
-
-// include the database script
-require_once 'dbconnect.php';
-// Return to home page if user not same
-if (!isset($_SESSION['usr_id'])) {
-    header("Location: index.php");
-    echo "''<h1>.Wrong User!.</h1>";
-}
-
-$select = mysqli_query($link, 'select * from students');
-$rows = array();
-
-while ($row=mysqli_fetch_array($select)){
-    $rows[] = $row;
-}
-
-
-?>
+* Created by PhpStorm.
+* User: Chisoft
+* Date: 2017-05-02
+* Time: 15:02
+*/
 
 <!DOCTYPE html>
 <html>
@@ -81,9 +59,27 @@ while ($row=mysqli_fetch_array($select)){
     <hr>
 </form>
 <div class="pageContent">
+
     <?php
+    // Start a session
+    ob_start();
+    session_start();
+
+    // include the database script
+    require_once 'dbconnect.php';
+    // Return to home page if user not same
+
+    $select = mysqli_query($link, 'select * from students');
+    $rows = array();
+
+    while ($row=mysqli_fetch_array($select)){
+        $rows[] = $row;
+        echo "$rows";
+    }
     echo json_encode($row);
     ?>
+
+
 </div>
 <footer>
     <?php include 'include/footer.php'; ?>
