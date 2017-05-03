58cc858052fd3f8e813c11858273a8cc529f02ff
chisoftltd <chisoftltd@gmail.com>
2017-05-03 17:15:46 +0100

    further improvement

--- api/ethicsjson.php
+++ api/ethicsjson.php
@@ -30,14 +30,14 @@
         <div class="collapse navbar-collapse" id="navbar1">
             <ul class="nav navbar-nav navbar-right">
                 <?php if (isset($_SESSION['usr_id'])) { ?>
-                    <li class="active"><a href="../signinindex.php">Home</a></li>
-                    <li><a href="../research.php">Research</a></li>
-                    <li><a href="../officerprojecttable.php">Ethics Approval Officers (EAO)</a></li>
-                    <li><a href="../administrator.php">Administrator</a></li>
+                    <li class="active"><a href="../../signinindex.php">Home</a></li>
+                    <li><a href="../../research.php">Research</a></li>
+                    <li><a href="../../officerprojecttable.php">Ethics Approval Officers (EAO)</a></li>
+                    <li><a href="../../administrator.php">Administrator</a></li>
                     <li><a href="ethicsjson.php">EthicsJSON</a></li>
-                    <li><a href="../about.php">About Us</a></li>
+                    <li><a href="../../about.php">About Us</a></li>
                     <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
-                    <li><a href="../logout.php">Log Out</a></li>
+                    <li><a href="../../logout.php">Log Out</a></li>
                 <?php } else { ?>
                     <li class="active"><a href="../index.php">Home</a></li>
                     <li><a href="../about.php">About Us</a></li>
