a9c0e250e8f870b6bc0446aad9559524ead49d9c
chisoftltd <chisoftltd@gmail.com>
2017-05-04 20:46:44 +0100

    Further improvement

--- api/researchers.php
+++ api/researchers.php
@@ -49,9 +49,102 @@ switch ($request_type) {
 
 function get_id_researcher($reseacher)
 {
+    global $datab;
     //select case statement
     $query = "SELECT id, name, email, date FROM researchers where id = '$reseacher'";
     $reply = array();
 
-    $result = mysqli_query($link, $query);
+    $result = mysqli_query($datab, $query);
+    trigger_error($datab, E_USER_WARNING);
+
+    if (mysqli_num_rows($query)) {
+        while ($row = mysqli_fetch_assoc($query)) {
+            $col1["Researcher ID"] = $row['id'];
+            $col1['Name'] = $row['name'];
+            $col1['Email'] = $row['email'];
+            $col1['Date'] = $row['date'];
+
+            $reply[] = $col1;
+
+            header('Content Type: application/json');
+            echo json_encode($reply);
+        }
+    } else {
+        header("HTTP/1.0 204 No Content Found");
+    }
+}
+
+function get_researcher()
+{
+    global $datab;
+    //select case statement
+    $query = "SELECT id, name, email, date FROM researchers";
+    $reply = array();
+
+    $result = mysqli_query($datab, $query);
+    trigger_error($datab, E_USER_WARNING);
+
+    if (mysqli_num_rows($query)) {
+        while ($row = mysqli_fetch_assoc($query)) {
+            $col1["Researcher ID"] = $row['id'];
+            $col1['Name'] = $row['name'];
+            $col1['Email'] = $row['email'];
+            $col1['Date'] = $row['date'];
+
+            $reply[] = $col1;
+
+            header('Content Type: application/json');
+            echo json_encode($reply);
+        }
+    } else {
+        header("HTTP/1.0 204 No Content Found");
+    }
+}
+
+function insert_researcher($researcher)
+{
+    global $datab;
+    $password = generate();
+    $pwd = "";
+
+    for ($i = 0; $i < count($password); $i) {
+        $pwd .= $password[rand(0, (count($password) - 1))];
+    }
+
+    $researcherid = $researcher[0];
+    $researchername = $researcher[1];
+    $researcheremail = $researcher[2];
+    $researcherdate = new DateTime();
+
+    $query = "insert into researcher(id, name, email, password, date) VALUES ('$researcherid', '$researchername', '$researcheremail', '$pwd', '$researcherdate')";
+
+    $reply = array();
+    $result = mysqli_query($datab, $query);
+
+    if (mysqli_affected_rows($result) > 0) {
+        header("HTTP/1.0 201 Created Successfully");
+        echo json_encode($reply[0] = "researcher registered");
+    } else {
+        header("HTTP/1.0 409 Conflicting, Researcher ID Exists");
+        echo json_encode($reply[0] = "Researcher Exist, Please check the Researcher ID and try again");
+    }
+}
+
+function delete_researcher($researcher){
+    global $datab;
+    foreach ($researcher as $value) {
+
+        $query = "delete from researcher where id='$value'";
+        $result = mysqli_query($datab,$query) or die(trigger_error($datab, E_USER_WARNING));
+        mysqli_free_result($result);
+    }
+
+    $response = array();
+    if (mysqli_affected_rows($result) > 0) {
+        header("HTTP/1.0 201 Deleted Successfully");
+        echo json_encode($response[0]="Deleted successfully");
+    } else {
+        header("HTTP/1.0 204 No Content Found");
+
+    }
 }
