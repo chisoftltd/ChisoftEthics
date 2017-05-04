2e81b91b6f510fe007bf141db3611dab66667add
chisoftltd <chisoftltd@gmail.com>
2017-05-05 00:41:26 +0100

    further improvement

--- api/researchers.php
+++ api/researchers.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
             //print_r($reply);
             echo json_encode($reply);
-            //header('Content Type: application/json');
+            header('Content Type: application/json');
 
 
         }
