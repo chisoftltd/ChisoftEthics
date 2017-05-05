948d4edd82a8c50af4dc96bdf95b334bd875c9c9
chisoftltd <chisoftltd@gmail.com>
2017-05-05 04:37:55 +0100

    further improvement

--- api/researchers.php
+++ api/researchers.php
@@ -121,6 +121,7 @@ if ($_SERVER["REQUEST_METHOD"] == "PUT") {
             $query .= "password=$urlInfo[4]";
         }
         $query .= "date = now() where id=$urlInfo[1]";
+        echo $query. "  ";
         $result = mysqli_query($link, $query);
     } else {
         header("HTTP/1.0 40, researcher ID Not found");
