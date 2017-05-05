e3860b12ce9be6d1f22974044d2c752c0478fb93
chisoftltd <chisoftltd@gmail.com>
2017-05-05 15:41:18 +0100

    further improvement

--- api/researchers.php
+++ api/researchers.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
     $number = $urlInfo[1];
 
-    if (isset($urlInfo[1])) {
+    if (($urlInfo[1]) > 0 && ($urlInfo[1]) < 100)  {
 
         $queryID = "SELECT * FROM researchers where id = '$number'";
 
