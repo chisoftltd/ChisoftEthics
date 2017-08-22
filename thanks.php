8565197e2b8ca5dc5c0608e240ec72d38bd319c7
chisoftltd <chisoftltd@gmail.com>
2017-08-22 21:51:35 +0100

    Further improvement

---
+++ thanks.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1609963
+ * Date: 22/08/2017
+ * Time: 21:48
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head><!-- Head area start-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>RGUEthics | Contact US</title>
+    <link rel="stylesheet" href="css/main-style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
+    <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <!--<script src="http://maps.google.com/maps/api/js?sensor=false"></script>-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCSMxRAhrNG_VHJdpz0h72CyugKoMmDMQU&callback=init_map"></script>
+</head>
+<body><!-- Body area start-->
+<!-- add top navigational bar using bootstrap-->
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="contact.php">RGUEthics | Contact US</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- check if same user is still same as the active session user and load appropriate menu options -->
+                <?php if (isset($_SESSION['usr_id'])) { ?>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="officerprojecttable.php">Ethics Approval Officers (EAO)</a></li>
+                    <li><a href="registerresearcher.php">Register Researcher</a></li>
+                    <li class="active"><a href="contact.php">Contact</a></li>
+                    <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                <?php } else { ?>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li class="active"><a href="contact.php">Contact</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="registerresearcher.php">Register Researcher</a></li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- add header with navigational bar which interface depends on if there is an active user or not-->
+<header>
+    <?php if (isset($_SESSION['usr_id'])) { ?>
+        <?php include 'include/signinheader.php'; ?>
+
+    <?php } else { ?>
+        <?php include 'include/header.php'; ?><?php } ?>
+</header>
+<form>
+    <hr>
+</form>
+<div class="container"><!-- div for accepting messages-->
+    <h1 style="text-align: center">Thank you for contact us. We will get back to you soon!</h1>
+    <p "text-align: center">For urgent issues, call us on 075-79044346 </p>
+</div><!-- end of content div-->
+<!-- footer area-->
+<footer>
+    <?php include 'include/footer.php'; ?>
+</footer>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
