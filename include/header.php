c84485eb302d0aa68c4315a139a38e63b63376df
chisoftltd <chisoftltd@gmail.com>
2017-08-23 20:17:11 +0100

    Further improvement

--- include/header.php
+++ include/header.php
@@ -6,14 +6,4 @@ Header information when user is not sign on or logged in, with company logo
     <div id="logo">
         <a href="index.php"><img src="images/RGUEthics.png" alt="Company logo" /></a>
     </div>
-    <nav>
-        <ul class="header-links">
-            <li> <a href="index.php">Home</a></li>
-            <li> <a href="about.php">About Us</a></li>
-            <li><a href="api/researchers.php">EthicsJSON</a></li>
-            <li> <a href="contact.php">Contact</a></li>
-            <li> <a href="login.php">Login</a></li>
-            <li><a href="registerresearcher.php">Register Researcher</a></li>
-        </ul>
-    </nav>
 </header>
